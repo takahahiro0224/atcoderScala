478de95dc3ea324d94d36769e9b760cfb13cad4b
takahahiro0224 <hikusema189@keio.jp>
2020-06-04 01:46:09 +0900

    abc169_a

---
+++ src/main/scala/abc169/abc169_a.scala
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc169/tasks/abc169_a
+
+import java.util.Scanner
+
+object Main extends App {
+  println(1)
+  val sc = new Scanner(System.in)
+  val a, b = sc.nextInt
+  println(a*b)
+}
